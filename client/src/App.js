778427b2a47e390e6e9e512536866b3ce51bdd9e
subhash adhiakri <np03cs4a230438@heraldcollege.edu.np>
2025-05-21 00:48:56 +0545

    Fix: fully removed submodule reference for client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-600">
+  Tailwind is working!
+  Helllo eveyone
+</div>
+
+
+  );
+}
+
+export default App;
